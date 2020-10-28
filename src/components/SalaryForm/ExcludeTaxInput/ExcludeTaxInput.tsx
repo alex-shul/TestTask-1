5d161b6aec7b485eb4609c17723c180049286fce
alex-shul <Knight55555@yandex.ru>
2020-10-28 23:50:39 +0300

    contain-tax in classNames replaced with exclude-tax

--- src/components/SalaryForm/ExcludeTaxInput/ExcludeTaxInput.tsx
+++ src/components/SalaryForm/ExcludeTaxInput/ExcludeTaxInput.tsx
@@ -7,17 +7,17 @@ import PropTypes from "prop-types";
 const ExcludeTaxInput = ({ input: { value, onChange } }: WrappedFieldProps) => {
   return (
     <Form.Group as={Row} className='mb-0 ml-2'>
-      <Form.Label className='contain-tax__label small pl-0 pr-0' column xs='auto' htmlFor='contain-tax'>
-        <span className={'contain-tax__label-off' + (!value ? ' active' : '')}>Указать с НДФЛ</span>
+      <Form.Label className='exclude-tax__label small pl-0 pr-0' column xs='auto' htmlFor='exclude-tax'>
+        <span className={'exclude-tax__label-off' + (!value ? ' active' : '')}>Указать с НДФЛ</span>
         <Form.Check
           checked={value}
           onChange={() => onChange(!value)}
-          name='contain-tax'
+          name='exclude-tax'
           inline
           type='switch'
-          id='contain-tax'
+          id='exclude-tax'
         />
-        <span className={'contain-tax__label-on' + (value ? ' active' : '')}>Без НДФЛ</span>
+        <span className={'exclude-tax__label-on' + (value ? ' active' : '')}>Без НДФЛ</span>
       </Form.Label>
     </Form.Group>
   );
