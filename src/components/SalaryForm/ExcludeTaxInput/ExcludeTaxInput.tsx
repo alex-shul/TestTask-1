b6b2ed41257454612318770c26732a536ec14d62
alex-shul <Knight55555@yandex.ru>
2020-10-28 23:31:48 +0300

    exclude tax label fix + tips inner padding change

--- src/components/SalaryForm/ExcludeTaxInput/ExcludeTaxInput.tsx
+++ src/components/SalaryForm/ExcludeTaxInput/ExcludeTaxInput.tsx
@@ -8,7 +8,7 @@ const ExcludeTaxInput = ({ input: { value, onChange } }: WrappedFieldProps) => {
   return (
     <Form.Group as={Row} className='mb-0 ml-2'>
       <Form.Label className='contain-tax__label small pl-0 pr-0' column xs='auto' htmlFor='contain-tax'>
-        <span className='contain-tax__label-off'>Указать с НДФЛ</span>
+        <span className={'contain-tax__label-off' + (!value ? ' active' : '')}>Указать с НДФЛ</span>
         <Form.Check
           checked={value}
           onChange={() => onChange(!value)}
@@ -17,7 +17,7 @@ const ExcludeTaxInput = ({ input: { value, onChange } }: WrappedFieldProps) => {
           type='switch'
           id='contain-tax'
         />
-        <span className='contain-tax__label-on'>Без НДФЛ</span>
+        <span className={'contain-tax__label-on' + (value ? ' active' : '')}>Без НДФЛ</span>
       </Form.Label>
     </Form.Group>
   );
